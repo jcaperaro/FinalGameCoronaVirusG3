089c367a67c60285ec4cb7a46df7bac8cb8a07bf
Diego Gonzalez R <diegol.gonzalez@hotmail.com>
2020-11-19 08:21:40 -0500

    Diego G - Creacion de un script que se agrega al borde del juego para que detecte cuando choca o pasa el enemigo y la idea es eliminar el componente del Player, pero no me es posible lograrlo, no me muestra ni un mensaje en consola

---
+++ FinalGame_3G/Assets/Scripts/Border/BorderCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class BorderCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.CompareTag("Player")){
+            Debug.Log("Me han chocado");
+        }   
+    }
+}
