d6a3c6fdb38153862bb42420d183792d29cc7f28
Diego Gonzalez R <diegol.gonzalez@hotmail.com>
2020-11-19 07:51:57 -0500

    Diego G - Se agrega script de seguimiento de Cámera, se modifica bordes del juego para que no tenga borde de colision y permita que el personaje se caiga al vacio cuando caiga

---
+++ FinalGame_3G/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //variable para capturar el gameobject a seguir
+    public GameObject followObjectTarget;
+
+    //variables para suavizar el seguimiento de la camara
+    private float smoothTime = 0.25f;
+    private Vector2 velocity;
+
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //hago equivalencia con la ubicacion de la camara y el gameobject a seguir
+        float posX = Mathf.SmoothDamp
+            (transform.position.x
+            , followObjectTarget.transform.position.x
+            , ref velocity.x
+            , smoothTime);
+        float posY = Mathf.SmoothDamp
+            (transform.position.y
+            , followObjectTarget.transform.position.y
+            , ref velocity.y
+            , smoothTime);
+
+        //modifico la posicion de la camara con la del objeto a seguir
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
