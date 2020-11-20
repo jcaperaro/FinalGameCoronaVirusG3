a1aa9d586a0b93522f53359bf50158a0e42242ae
Diego Gonzalez R <diegol.gonzalez@hotmail.com>
2020-11-20 00:40:17 -0500

    Diego G - Creacion de poderes, con animacion al recogerlos, y tambien animacion en enemigo de prueba (poligono) que elimina al personaje cuando lo toca

---
+++ FinalGame_3G/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour{
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.transform.root.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ FinalGame_3G/Assets/Scripts/Power/capturePower.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class capturePower : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.transform.root.gameObject.CompareTag("Player")){
+            GetComponent<SpriteRenderer>().enabled = false;
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            Destroy(gameObject, 1f);
+        }
+    }
+}
