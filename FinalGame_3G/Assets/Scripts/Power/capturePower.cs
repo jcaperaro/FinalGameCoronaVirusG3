fbb4d5a4f9ef1b0aa5a540b60e8c8de26100b7aa
Diego Gonzalez R <diegol.gonzalez@hotmail.com>
2020-11-30 11:34:30 -0500

    Diego G - Se cambia la forma en que se agrega la música de fondo al (Level1) ahora lo hago por medio de un componente asignado al GameObject (AudioManager) y ahí se le carga la cancion de fondo, se pueden modificar controles.. ///  Tambien le agrego componente de sonido al Prefab del (Power) para que cada vez que lo toquen, haga un efecto de sonido

--- FinalGame_3G/Assets/Scripts/Power/capturePower.cs
+++ FinalGame_3G/Assets/Scripts/Power/capturePower.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 /*Script que permite que los (poderes - Power) tengan un trigger, que detecten cuando el personaje los toque, de inmediato, se ocultará el (Sprite Renderer) del poder, 
  y queda en valor (false), luego como debajo del (Poder) está la animación de cuando cogen el poder, pero está apagada desde el inicio, pues de inmediato cuando se apaga el poder,
  se prende el (Game Object) del (capture Power), lo cual crea la ilusión de que hay un efecto cuando cogen el poder.... y después de 1 segundo se elimina este game object del
  (capture Power)*/
 public class capturePower : MonoBehaviour{
+    public AudioSource sound;
     private void OnTriggerEnter2D(Collider2D collision){
         if (collision.transform.root.gameObject.CompareTag("Player")){
-            //AudioManager.p_Instance.PlayMusic(Poder);
+            sound.Play();
             GetComponent<SpriteRenderer>().enabled = false;
             gameObject.transform.GetChild(0).gameObject.SetActive(true);
             Destroy(gameObject, 1f);
