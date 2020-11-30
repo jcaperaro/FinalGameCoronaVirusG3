5588f58a4793d61a56fe73e236186967701cc4f4
Diego Gonzalez R <diegol.gonzalez@hotmail.com>
2020-11-30 00:41:25 -0500

    Diego G - Se agregan por el momento audio de fondo al nivel 1, y también efecto de sonido cuando el personaje salta... OJO, solo se está haciendo uso del archivo (AudioManagerTest.cs), el del componente (AudioManager) que tiene asociado el archivo (AudioManager.cs) se encuentran deshabilitados

---
+++ FinalGame_3G/Assets/Scripts/Sound/AudioManagerTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagerTest : MonoBehaviour{
+    [SerializeField] private AudioClip sfx;
+    [SerializeField] private AudioClip music1;
+    [SerializeField] private AudioClip music2;
+
+    private void Update(){
+        if (Input.GetButtonDown("Vertical"))
+            AudioManager.p_Instance.PlaySFX(sfx);
+    }
+
+    void Start() {
+        AudioManager.p_Instance.PlayMusic(music2);
+    }
+}
