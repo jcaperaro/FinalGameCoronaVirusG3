6dbfaa02651a729edcef803d9bd97a35ae6d2d7c
unknown <dsarmientolo@uninpahu.edu.co>
2020-12-01 00:18:08 -0500

    Se crea menu principal para el juego y menu para seleccionar niveles

---
+++ FinalGame_3G/Assets/Scripts/Buttons/SelectLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectLevel : MonoBehaviour
+{
+    public void selectLevel() 
+    {
+        switch (this.gameObject.name) 
+        {
+            case "LevelOne":
+                SceneManager.LoadScene("Level1");
+                break;
+            case "LevelTwo":
+                SceneManager.LoadScene("Level2");
+                break;
+        }
+    }
+}
